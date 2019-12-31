92bf2ac2305f729327c9129c3ba4094721914d38
difficulttochoose <pmd27041997@gmail.com>
2019-12-31 21:09:44 +0200

    init commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+//Rick and Morty API - Analize!!! Count... Filter...
+axios
+    .get('https://rickandmortyapi.com/api/character/',)
+    .then(res => {
+        const {pages} = res.data.info
+        localStorage.setItem('password', 'super strong password')
+
+        Promise.all([...new Array(pages)].map((el,i) => axios.get(`https://rickandmortyapi.com/api/character/?page=${i+1}`)))
+            .then(res => console.log(res));
+            
+    })
